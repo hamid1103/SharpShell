b34da77ae7099321bcd27fa66835302f0cffba6e
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2026-03-19 20:06:18 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,8 +2,11 @@ class Program
 {
     static void Main()
     {
-        Console.Write("$ ");
-        string input = Console.ReadLine();
-        Console.Write($"{input}: command not found");
+        while (true)
+        {
+            Console.WriteLine("$ ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"{input}: command not found");   
+        }
     }
 }
