848ab2f855950f84d850323942f5c78b0c0e2e13
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2026-03-19 20:02:51 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,7 +2,8 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        string input = Console.ReadLine();
+        Console.Write($"{input}: command not found");
     }
 }
